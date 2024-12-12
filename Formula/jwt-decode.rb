7e7e5f75b04322be811bcd3b699a6592c3780d7c
jadolg <diazorozcoj@gmail.com>
2024-12-12 08:46:52 +0100

    Brew formula update for jwt-decode version v2.1.5

--- Formula/jwt-decode.rb
+++ Formula/jwt-decode.rb
@@ -5,20 +5,20 @@
 class JwtDecode < Formula
   desc "Simple tool to decode JSON Web token."
   homepage "https://github.com/jadolg/jwt-decode"
-  version "2.1.4"
+  version "2.1.5"
 
   on_macos do
     on_intel do
-      url "https://github.com/jadolg/jwt-decode/releases/download/v2.1.4/jwt-decode_Darwin_x86_64.tar.gz"
-      sha256 "b29b00798f9d3d63458074927e08ea931ca05ba868df48144396516cb346c7d0"
+      url "https://github.com/jadolg/jwt-decode/releases/download/v2.1.5/jwt-decode_Darwin_x86_64.tar.gz"
+      sha256 "d188307c6c3f3a3f3c149098ae3f162841674f835106afa7fedce4abda596bee"
 
       def install
         bin.install "jwt-decode"
       end
     end
     on_arm do
-      url "https://github.com/jadolg/jwt-decode/releases/download/v2.1.4/jwt-decode_Darwin_arm64.tar.gz"
-      sha256 "357590422726256e3f1cd8bf17a87fab55af94e5b94920b30a5535c1a59b1bb4"
+      url "https://github.com/jadolg/jwt-decode/releases/download/v2.1.5/jwt-decode_Darwin_arm64.tar.gz"
+      sha256 "1a0f6ceec15a2910c34e634ff4fdbf6436895061ea3463a318ab174f120876f7"
 
       def install
         bin.install "jwt-decode"
@@ -29,8 +29,8 @@ class JwtDecode < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/jadolg/jwt-decode/releases/download/v2.1.4/jwt-decode_Linux_x86_64.tar.gz"
-        sha256 "d172dd9daa680a96dfe0c55e862687337baaaa29a80ccb2d0dc2eb5d5547a22f"
+        url "https://github.com/jadolg/jwt-decode/releases/download/v2.1.5/jwt-decode_Linux_x86_64.tar.gz"
+        sha256 "2989bb20c5ac555c4be977e4d0ab752c94608babf828a5c0b4ff7909e8ed7e8d"
 
         def install
           bin.install "jwt-decode"
@@ -39,8 +39,8 @@ class JwtDecode < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/jadolg/jwt-decode/releases/download/v2.1.4/jwt-decode_Linux_arm64.tar.gz"
-        sha256 "4e19d707523de256d040d64e9144ce561976c2f2431e18a1e1404c07eb9219c4"
+        url "https://github.com/jadolg/jwt-decode/releases/download/v2.1.5/jwt-decode_Linux_arm64.tar.gz"
+        sha256 "83a7d356c74c167a582d8235fbe00c5deef8319dd8d140b19915a6fdffbdbaec"
 
         def install
           bin.install "jwt-decode"
